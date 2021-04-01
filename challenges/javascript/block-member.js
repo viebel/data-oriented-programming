820794ae7c1be6226a9a759b1a5a058e54f35096
Yehonathan Sharvit <viebel@gmail.com>
2021-04-01 19:11:16 +0300

    Update block-member.js

--- challenges/javascript/block-member.js
+++ challenges/javascript/block-member.js
@@ -1,9 +1,11 @@
-function blockMember(email) {
+function blockMember(libraryData, email) {
   var informationPath = ["userManagement", "members", email, "isBlocked"]; 
   return _.set(libraryData, informationPath, true);
 }
 
-var updatedLibraryData = blockMember("samantha@gmail.com");
+var updatedLibraryData = blockMember(libraryData, "samantha@gmail.com");
 
 var informationPath = ["userManagement", "members", "samantha@gmail.com", "isBlocked"]; 
 [_.get(updatedLibraryData, informationPath), _.get(libraryData, informationPath)];
+
+
