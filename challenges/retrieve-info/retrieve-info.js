7480ce7139b02830b1b7983139122421be61e152
Yehonathan Sharvit <viebel@gmail.com>
2021-04-01 18:55:06 +0300

    Delete retrieve-info.js

--- challenges/retrieve-info/retrieve-info.js
+++
@@ -1,6 +0,0 @@
-function getBookProperty(isbn, fieldName) {
-  var informationPath = ["catalog", "booksByIsbn", isbn, fieldName]; 
-  return _.get(libraryData, informationPath);
-}
-
-getBookProperty("978-1779501127", "title");
