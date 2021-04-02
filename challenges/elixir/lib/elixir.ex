8f5576526d90cc75c5ff6e533027b6931edc3064
Daniel Khaapamyaki <daniel.khaapamyaki@smartvokat.com>
2021-04-03 00:47:22 +0200

    feat: Initialize new elixir project
    `mix new elixir`

---
+++ challenges/elixir/lib/elixir.ex
@@ -0,0 +1,18 @@
+defmodule Elixir do
+  @moduledoc """
+  Documentation for `Elixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
