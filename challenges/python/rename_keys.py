2446c8963913b32a5d409bbd6b0ed2c3d4280f35
shaolang <shaolang@ymail.com>
2021-04-03 11:26:42 +0800

    Complete challenge 4: rename keys in a data entity

---
+++ challenges/python/rename_keys.py
@@ -0,0 +1,34 @@
+import copy
+
+def rename_keys(m, key_map):
+    return {key_to_use(key_map, k): v for (k, v) in m.items()}
+
+
+def key_to_use(key_map, k):
+    if k in key_map:
+        return key_map[k]
+    else:
+        return k
+
+
+def show(original, updated):
+    print(f'original:{original}')
+    print(f'updated:{updated}\n')
+
+
+if __name__ == '__main__':
+    alan_moore = {
+            'name': 'Alan Moore',
+            'bookIsbns': ['978-1779501127']}
+
+    updated_alan = rename_keys(alan_moore, {'bookIsbns': 'books'})
+    show(alan_moore, updated_alan)
+
+
+    book_item = {
+            'id': 'book-item-1',
+            'rackId': 'rack-17',
+            'isLent': True}
+
+    updated_book_item = rename_keys(book_item, {'rackId': 'id', 'id': 'bookItemId'})
+    show(book_item, updated_book_item)
