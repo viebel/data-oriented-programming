afeb1dc93b0029ec763d00445b0ed6ca55410631
shaolang <shaolang@ymail.com>
2021-04-04 16:34:12 +0800

    Implement lodash-like get function for get_book_property

--- challenges/python/retrieve_information.py
+++ challenges/python/retrieve_information.py
@@ -1,5 +1,10 @@
 def get_book_property(library_data, isbn, field_name):
-    return library_data['catalog']['booksByIsbn'][isbn][field_name]
+    return __get(library_data, ['catalog', 'booksByIsbn', isbn, field_name])
+
+
+def __get(m, ks):
+    return m[ks[0]] if len(ks) == 1 else __get(m[ks[0]], ks[1:])
+
 
 if __name__ == '__main__':
     from library_data import data
