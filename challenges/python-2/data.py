5704561ab1349e7001d8b162349d47beeb475a64
Sebastian Hofer <sebastian.hofer@gmx.net>
2022-01-26 13:32:20 +0100

    Standardize the quotes used in the data.

--- challenges/python-2/data.py
+++ challenges/python-2/data.py
@@ -1,50 +1,50 @@
 
 library_data = {
-    "name": "The smallest library on earth",
-    "address": "Here and now",
-    "catalog": {
-        "booksByIsbn": {
-            "978-1779501127": {
-                "isbn": "978-1779501127",
-                "title": "Watchmen",
-                "publicationYear": 1987,
-                "authorIds": ["alan-moore",
-                              "dave-gibbons"],
-                "bookItems": [
+    'name': 'The smallest library on earth',
+    'address': 'Here and now',
+    'catalog': {
+        'booksByIsbn': {
+            '978-1779501127': {
+                'isbn': '978-1779501127',
+                'title': 'Watchmen',
+                'publicationYear': 1987,
+                'authorIds': ['alan-moore',
+                              'dave-gibbons'],
+                'bookItems': [
                     {
-                        "id": "book-item-1",
-                        "rackId": "rack-17"
+                        'id': 'book-item-1',
+                        'rackId': 'rack-17'
                     },
                     {
-                        "id": "book-item-2",
-                        "rackId": "rack-17"
+                        'id': 'book-item-2',
+                        'rackId': 'rack-17'
                     }
                 ]
             }
         },
-        "authorsById": {
-            "alan-moore": {
-                "name": "Alan Moore",
-                "bookIsbns": ["978-1779501127"]
+        'authorsById': {
+            'alan-moore': {
+                'name': 'Alan Moore',
+                'bookIsbns': ['978-1779501127']
             },
-            "dave-gibbons": {
-                "name": "Dave Gibbons",
-                "bookIsbns": ["978-1779501127"]
+            'dave-gibbons': {
+                'name': 'Dave Gibbons',
+                'bookIsbns': ['978-1779501127']
             }
         }
     },
-  "userManagement": {
-    "librarians": {
-      "franck@gmail.com" : {
-        "email": "franck@gmail.com",
-        "encryptedPassword": "bXlwYXNzd29yZA=="
+  'userManagement': {
+    'librarians': {
+      'franck@gmail.com' : {
+        'email': 'franck@gmail.com',
+        'encryptedPassword': 'bXlwYXNzd29yZA=='
       }
     },
-    "members": {
-      "samantha@gmail.com": {
-        "email": "samantha@gmail.com",
-        "encryptedPassword": "c2VjcmV0",
-        "isBlocked": False
+    'members': {
+      'samantha@gmail.com': {
+        'email': 'samantha@gmail.com',
+        'encryptedPassword': 'c2VjcmV0',
+        'isBlocked': False
       }
     }
   }
@@ -52,9 +52,9 @@ library_data = {
 
 
 watchmen_book_info = {
-  "title": "Watchmen",
-  "isbn": "978-1779501127",
-  "authorNames": ["Alan Moore", "Dave Gibbons"]
+  'title': 'Watchmen',
+  'isbn': '978-1779501127',
+  'authorNames': ['Alan Moore', 'Dave Gibbons']
 }
 
 watchmen_from_db = {
